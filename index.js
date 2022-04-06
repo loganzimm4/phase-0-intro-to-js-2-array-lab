fa2809dd964f628ecc2747f995b58b1599e767c9
loganzimm4 <101672520+loganzimm4@users.noreply.github.com>
2022-04-06 13:38:43 -0400

    commit tree 7f6e3259adae0cff90e9c8e05c0d19c1a3906743

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    let newCats = cats.slice();
+    newCats.push('Broom');
+    return newCats;
+}
+
+function prependCat() {
+    let newCats = cats.slice();
+    newCats.unshift('Arnold');
+    return newCats;
+}
+
+function removeLastCat() {
+    let newCats = cats.slice();
+    newCats.pop();
+    return newCats;
+}
+
+function removeFirstCat() {
+    let newCats = cats.slice();
+    newCats.shift();
+    return newCats;
+}
